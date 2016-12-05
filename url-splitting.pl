de92792a219dba6ac3b5f3beb974eb1de20bf742
Yann Masson <yann.masson@orange.fr>
2016-12-05 22:59:01 +0100

    Stages test

--- url-splitting.pl
+++ url-splitting.pl
@@ -3,4 +3,38 @@
 use strict;
 use warnings;
 
+my $param;
 
+$param = @_;
+print ("$param \n");
+
+sub get_protocol
+{
+  my $i = shift;
+  print("$i \n");
+  my $string = '://';
+  my @split_res = split(/$string/, $i);
+  my $split_res;
+  print("$split_res[0] \n");
+  print ("protocol : $split_res[0]\n");
+}
+
+sub get_domain
+{
+  my $i = shift;
+  print($i);
+  my $string = '://';
+  my @split_res = split(/$string/, $i);
+  my $split_res;
+  print($split_res[1]);
+  print ("domain   : $split_res[1]");
+}
+
+
+get_protocol($param);
+get_domain($param);
+
+#print("protocol : http\n");
+#print("domain   : www.test.com\n");
+
+exit 0;
